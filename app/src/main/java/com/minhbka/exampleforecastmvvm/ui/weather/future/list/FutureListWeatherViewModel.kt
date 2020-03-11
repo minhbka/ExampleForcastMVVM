bc3e98831955e46f1f511582175676e51062bb46
Phan Minh <minhpv@kormedi.com>
2020-03-11 15:25:31 +0900

    Navigation & App Foundation

---
+++ app/src/main/java/com/minhbka/exampleforecastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.minhbka.exampleforecastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel;
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
